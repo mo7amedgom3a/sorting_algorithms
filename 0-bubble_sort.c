e92a474f381a48277045efcde19b41cb2c6c0863
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-10-24 00:41:13 +0300

    test

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,24 +23,24 @@ void swap_ints(int *a, int *b)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, len = size;
-	bool bubbly = false;
+	size_t i = 0;
+	bool swapped = true;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (swapped)
 	{
-		bubbly = true;
-		for (i = 0; i < len - 1; i++)
+		swapped = false;
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
-				bubbly = false;
+				swapped = true;
 			}
 		}
-		len--;
+		i++;
 	}
 }
